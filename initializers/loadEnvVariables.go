6aa4ae5688f80a9406062a74e39cc90391823c6b
Haidarr-h <haidarapps12@gmail.com>
2026-02-20 14:28:47 +0700

    refactor the env variable load

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -10,6 +10,6 @@ func LoadEnvVariables() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Println("Error loading .env file")
+		log.Println("No .env file found, using system environment variables")
 	}
 }
