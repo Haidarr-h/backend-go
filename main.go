0db4970cd3f458afa82dba40cfc0ab93c8ed77c3
Haidarr-h <haidarapps12@gmail.com>
2026-03-17 21:04:53 +0700

    fix cors for dev

--- main.go
+++ main.go
@@ -35,10 +35,10 @@ func main() {
 
 	// CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		AllowCredentials: true,
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+		// AllowCredentials: true
 	}))
 
 	// ROUTE
