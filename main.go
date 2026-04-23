8de7b692b516b40ce62501f444580f825ab0a992
Haidarr-h <haidarapps12@gmail.com>
2026-04-23 14:04:22 +0700

    add logging

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Haidarr-h/backend-go/config"
 	"github.com/Haidarr-h/backend-go/controllers"
 	_ "github.com/Haidarr-h/backend-go/docs"
+	"github.com/Haidarr-h/backend-go/pkg/logger"
 	"github.com/Haidarr-h/backend-go/routes"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -24,6 +25,7 @@ import (
 func init() {
 	config.LoadEnvVariables()
 	config.InitDB()
+	logger.Init()
 }
 
 func main() {
